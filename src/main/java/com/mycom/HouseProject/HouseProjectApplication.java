e1c9f4f0c44fd3df03488d3bff2d3cf42cb0771e
jini_11 <jieun21124@naver.com>
2022-04-06 17:21:45 +0900

    init

---
+++ src/main/java/com/mycom/HouseProject/HouseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.HouseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycom/HouseProject/HouseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.HouseProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
