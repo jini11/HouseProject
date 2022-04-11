c897e93fd1a355d82974ff3f8b638e8474b29233
jini_11 <jieun21124@naver.com>
2022-04-11 17:32:18 +0900

    아이디, 회원가입 페이지

---
+++ src/main/java/com/mycom/HouseProject/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.mycom.HouseProject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "account/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "account/register";
+    }
+
+}
