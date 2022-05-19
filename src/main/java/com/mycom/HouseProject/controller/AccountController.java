52c75c74ce8cd4d9da378924c48bc78ba511eba0
jini_11 <jieun21124@naver.com>
2022-05-19 15:51:43 +0900

    관리자 회원 정보 관리

--- src/main/java/com/mycom/HouseProject/controller/AccountController.java
+++ src/main/java/com/mycom/HouseProject/controller/AccountController.java
@@ -5,6 +5,9 @@ import com.mycom.HouseProject.model.User;
 import com.mycom.HouseProject.repository.UserRepository;
 import com.mycom.HouseProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -60,5 +63,16 @@ public class AccountController {
         String userid = authentication.getName();
         userService.save(userid, user);
         return "redirect:/account/info";
+    }
+
+    @GetMapping("/admin_info")
+    public String admin_info(Model model, @PageableDefault(size = 10) Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
+        int startPage = Math.max(1, users.getPageable().getPageNumber() - 4 );
+        int endPage = Math.min(users.getTotalPages(), users.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("users", users);
+        return "/account/admin_info";
     }
 }
--- src/main/java/com/mycom/HouseProject/repository/UserRepository.java
+++ src/main/java/com/mycom/HouseProject/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.mycom.HouseProject.repository;
 
 import com.mycom.HouseProject.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
