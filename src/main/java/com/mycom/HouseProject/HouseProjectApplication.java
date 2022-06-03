ae9866ef62f054551b63cfbde249a82cd3ef284e
jini_11 <jieun21124@naver.com>
2022-06-03 23:34:34 +0900

    호스팅 준비,,

--- src/main/java/com/mycom/HouseProject/HouseProjectApplication.java
+++ src/main/java/com/mycom/HouseProject/HouseProjectApplication.java
@@ -2,12 +2,18 @@ package com.mycom.HouseProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HouseProjectApplication {
+public class HouseProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseProjectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HouseProjectApplication.class);
+	}
 }
